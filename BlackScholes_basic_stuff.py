29e5f2c1f1962c3c21d107be89136dae39d69b99
pra-kri <37740552+pra-kri@users.noreply.github.com>
2018-06-03 21:28:56 +0100

    Update BlackScholes_basic_stuff.py

--- BlackScholes_basic_stuff.py
+++ BlackScholes_basic_stuff.py
@@ -5,8 +5,16 @@ import math
 # into smaller chunks and save them as variables a, b, c
 
 
+# -------------------------+
+# S = stock price
+# K = strike price
+# r = risk-free rate
+# sigma = annualised volatility
+# T = time to expiration
+# -------------------------+
+
 def cdf_normal(x):
-    """ use the erf() function help build the cumulative dist function """
+    """ use the erf() function to make the cumulative normal dist. function """
     return (1.0 + erf(x/ sqrt(2.0))) / 2.0
 
 def d1(S, K, r, sigma, T):
@@ -19,22 +27,24 @@ def d1(S, K, r, sigma, T):
 
 
 def d2(S, K, r, sigma, T):
+    
     a2 = math.log(S/K) + T*(r - 0.5*sigma**2)
     b2 = sigma * math.sqrt(T)
     c2 = a2/b2
     
     return c2
 
+
 def Call(S, K, r, sigma, T):
-    """ Call Price """
+    """ Call Option Value """
     a3 = S*cdf_normal(d1(S, K, r, sigma, T))
     b3 = K*math.exp(-r*T)*cdf_normal(d2(S,K,r,sigma,T))
-    c3 = a3-b3
+    c3 = a3 - b3
     
     return c3
 
 def Put(S, K , r,sigma, T):
-    """ Put Price. 
+    """ Put Option Value. 
     Using the Call Price function within this, but should probably remove dependency for better code
     """
     temp_call = Call(S, K, r, sigma, T)
@@ -43,4 +53,5 @@ def Put(S, K , r,sigma, T):
     return temp_put
 
 # [] -TODO: neaten up and make more readable, this is just for practice from reading the Paul Wilmott textbook.
+# [] -TODO: remove unnecessary dependencies between the functions. Dependencies probably makes the code more fragile...
 # [] -TODO: the erf() 'error function' is a sigmoid... so can we use the same function in Neural Networks?.... check out later
